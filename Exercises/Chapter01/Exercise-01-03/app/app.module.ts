36576cf9f8fbb46acaef0c41eae18ab83a5c578c
mtraina <matteo.traina.mail@gmail.com>
2016-12-25 19:58:51 +0100

    Exercise 01-03

---
+++ Exercises/Chapter01/Exercise-01-03/app/app.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {DiveLogComponent} from './dive-log.component';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [DiveLogComponent],
+    bootstrap: [DiveLogComponent]
+})
+export class AppModule {}
---
+++ Exercises/Chapter01/Exercise-01-03/app/dive-log.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'divelog',
+    templateUrl: 'app/dive-log.template.html'
+})
+export class DiveLogComponent {
+    public dives = [
+        {
+            site: 'Abu Gotta Ramada',
+            location: 'Hurgada, Egypt',
+            depth: 72,
+            time: 54
+        },
+        {
+            site: 'Ponte Mahoon',
+            location: 'Maehbourg, Mauritius',
+            depth: 54,
+            time: 38
+        },
+        {
+            site: 'Molnar Cave',
+            location: 'Budapest, Hungary',
+            depth: 98,
+            time: 62
+        }
+    ];
+}
