86a874be3320ad04465e119e7e51f29178649035
mtraina <matteo.traina.mail@gmail.com>
2016-12-30 15:12:59 +0100

    dive log entry type added

---
+++ Exercises/Chapter01/Exercise-01-06/app/dive-log-entry.ts
@@ -0,0 +1,26 @@
+export class DiveLogEntry {
+    site: string;
+    location: string;
+    depth: number;
+    time: number;
+
+    static StockDives: DiveLogEntry[] = [
+    {
+        site: 'Abu Gotta Ramada',
+        location: 'Hurgada, Egypt',
+        depth: 72,
+        time: 54
+    },
+    {
+        site: 'Ponte Mahoon',
+        location: 'Maehbourg, Mauritius',
+        depth: 54,
+        time: 38
+    },
+    {
+        site: 'Molnar Cave',
+        location: 'Budapest, Hungary',
+        depth: 98,
+        time: 62
+    }];
+}
