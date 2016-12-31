4ace4dcf7d5cedb509cd93ac2cf3dfcf97e70785
mtraina <matteo.traina.mail@gmail.com>
2016-12-31 14:48:49 +0100

    add site implementation

--- Exercises/Chapter02/Exercise-02-04/app/add-site.component.ts
+++ Exercises/Chapter02/Exercise-02-04/app/add-site.component.ts
@@ -1,15 +1,17 @@
-import {Component, Output, EventEmitter} from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
     selector: 'add-site-view',
     templateUrl: 'app/add-site.template.html'
 })
 export class AddSiteComponent {
-    @Output() onAdded = new EventEmitter();
+    @Input() siteId: number;
+    @Output() onAdded = new EventEmitter<string>();
     @Output() onCancel = new EventEmitter();
+    siteName: string;
 
     added(){
-        this.onAdded.emit(null);
+        this.onAdded.emit(this.siteName);
     }
 
     cancel(){
--- Exercises/Chapter02/Exercise-02-04/app/app.component.ts
+++ Exercises/Chapter02/Exercise-02-04/app/app.component.ts
@@ -7,9 +7,20 @@ import {DiveSite} from './dive-site';
 })
 export class AppComponent {
     sites = DiveSite.FavoriteSites.slice(0);
+    newSiteId: number;
     currentView = 'list';
 
     navigateTo(view: string){
         this.currentView = view;
+    }
+
+    startAdd(){
+        this.newSiteId = this.sites.map(s => s.id).reduce((p, c) => p < c ? c : p) + 1;
+        this.navigateTo('add');
+    }
+
+    siteAdded(newSiteName: string){
+        this.sites.push({id: this.newSiteId, name: newSiteName});
+        this.navigateTo('list');
     }
 }
