530c5c6d6785881bb4bdd16db41cfe8f58c1d2d3
mtraina <matteo.traina.mail@gmail.com>
2016-12-31 14:14:50 +0100

    navigation between list and add views I

---
+++ Exercises/Chapter02/Exercise-02-03/app/add-site.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+import {DiveSite} from './dive-site';
+
+@Component({
+    selector: 'add-site-view',
+    templateUrl: 'app/add-site.template.html'
+})
+export class AddSiteComponent {
+}
