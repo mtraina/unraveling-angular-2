72583eb4401f309a61923bd71ae6bd287a55b5ee
mtraina <matteo.traina.mail@gmail.com>
2017-01-07 20:31:07 +0100

    style in add site component

--- Exercises/Chapter02/Exercise-02-08/app/add-site.component.ts
+++ Exercises/Chapter02/Exercise-02-08/app/add-site.component.ts
@@ -2,7 +2,13 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
     selector: 'add-site-view',
-    templateUrl: 'app/add-site.template.html'
+    templateUrl: 'app/add-site.template.html',
+    styles: [`
+        h3 {
+            font-weight: bold;
+            color: maroon;
+        }
+    `]
 })
 export class AddSiteComponent {
     @Input() siteId: number;
