8da820c43b8df1aacb0625304e0db2f28f373782
mtraina <matteo.traina.mail@gmail.com>
2017-06-06 21:06:30 +0200

    base part of exercise running

---
+++ Exercises/Chapter07/Exercise-07-02/app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'yw-app',
+  template: `
+    <div class="container-fluid">
+      <h1>Find Underwater Token Game</h1>
+      <!-- <yw-game></yw-game> -->
+    </div>
+  `
+})
+export class AppComponent {
+}
--- Exercises/Chapter07/Exercise-07-02/app/app.module.ts
+++ Exercises/Chapter07/Exercise-07-02/app/app.module.ts
@@ -2,15 +2,15 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {GameComponent} from './game.component';
-import {DiverComponent} from './diver.component';
+//import {GameComponent} from './game.component';
+//import {DiverComponent} from './diver.component';
 
 @NgModule({
   imports: [BrowserModule],
   declarations: [
     AppComponent,
-    GameComponent,
-    DiverComponent
+    //GameComponent,
+    //DiverComponent
   ],
   bootstrap: [AppComponent]
 })
