bcea4aefe10ad8582ca2301eef18718dd6d0fae8
mtraina <matteo.traina.mail@gmail.com>
2017-06-08 20:22:03 +0200

    exrcise 08-03 creation

---
+++ Exercises/Chapter08/Exercise-08-03/app/app.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+
+import {AppComponent} from './app.component';
+import {DiveLogFormComponent} from './dive-log-form.component'
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    DiveLogFormComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
