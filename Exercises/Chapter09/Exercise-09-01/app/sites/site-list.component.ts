cde94795bcd44e950b8de88379d8cb2adcc0ecb8
mtraina <matteo.traina.mail@gmail.com>
2017-06-11 14:56:24 +0200

    exercise 09-01 creation

---
+++ Exercises/Chapter09/Exercise-09-01/app/sites/site-list.component.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+
+import {DiveSite} from './dive-site';
+import {SiteManagementService} from './site-management.service'
+
+@Component({
+  selector: 'site-list-view',
+  templateUrl: 'app/sites/site-list.template.html',
+  styleUrls: ['app/sites/site-list.styles.css']
+})
+export class SiteListComponent {
+  sites: DiveSite[];
+
+  constructor(
+    private siteService: SiteManagementService,
+    private router: Router
+  ) {
+    this.sites = siteService.getAllSites();
+  }
+}
---
+++ Exercises/Chapter09/Exercise-09-01/app/welcome/welcome.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'welcome',
+  templateUrl: 'app/welcome/welcome.template.html'
+})
+export class WelcomeComponent {
+}
