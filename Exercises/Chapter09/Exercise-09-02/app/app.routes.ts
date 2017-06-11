62be8de09a689a16be4141dd15c8c874850ee41f
mtraina <matteo.traina.mail@gmail.com>
2017-06-11 15:28:45 +0200

    sites routes added

--- Exercises/Chapter09/Exercise-09-02/app/app.routes.ts
+++ Exercises/Chapter09/Exercise-09-02/app/app.routes.ts
@@ -1,12 +1,13 @@
 import {Routes, RouterModule} from '@angular/router';
 import {WelcomeComponent} from './welcome/welcome.component';
 import {DiveLogComponent} from './logs/dive-log.component';
-import {SiteListComponent} from './sites/site-list.component';
+
+import {sitesRoutes} from './sites/sites.routing';
 
 const routes: Routes = [
   { path: 'divelogs', component: DiveLogComponent },
-  { path: 'sites', component: SiteListComponent },
-  { path: '', pathMatch: 'full', component: WelcomeComponent }
+  { path: '', pathMatch: 'full', component: WelcomeComponent },
+  ...sitesRoutes
 ];
 
 export const routingModule = RouterModule.forRoot(routes);
