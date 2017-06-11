2755973cf9c1dc1746b385f3dc676ac644d4865a
mtraina <matteo.traina.mail@gmail.com>
2017-06-11 15:29:50 +0200

    exercise 09-03 creation

---
+++ Exercises/Chapter09/Exercise-09-03/app/sites/sites.routing.ts
@@ -0,0 +1,19 @@
+import {Routes, RouterModule} from '@angular/router';
+import {SitesComponent} from './sites.component';
+import {SiteListComponent} from './site-list.component';
+import {AddSiteComponent} from './add-site.component';
+import {EditSiteComponent} from './edit-site.component';
+import {DeleteSiteComponent} from './delete-site.component';
+
+export const sitesRoutes: Routes = [{
+    path: 'sites',
+    component: SitesComponent,
+    children: [
+        { path: '', component: SiteListComponent },
+        { path: 'add', component: AddSiteComponent },
+        { path: 'edit/:id', component: EditSiteComponent },
+        { path: 'delete/:id', component: DeleteSiteComponent }
+    ]
+}];
+
+export const sitesRoutingModule = RouterModule.forChild(sitesRoutes);
