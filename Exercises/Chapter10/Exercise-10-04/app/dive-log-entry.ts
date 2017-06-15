946179a26cc995d6d075e8215bf9267d7f7bd4f6
mtraina <matteo.traina.mail@gmail.com>
2017-06-15 17:51:30 +0200

    date formatting pipe

--- Exercises/Chapter10/Exercise-10-04/app/dive-log-entry.ts
+++ Exercises/Chapter10/Exercise-10-04/app/dive-log-entry.ts
@@ -3,24 +3,28 @@ export class DiveLogEntry {
   location: string;
   depth: number;
   time: number;
+  divedOn: Date;
 
   static StockDives: DiveLogEntry[] = [
   {
     site: 'Abu Gotta Ramada',
     location: 'Hurghada, Egypt',
     depth: 72,
-    time: 54
+    time: 54,
+    divedOn: new Date("9/27/2012 11:25")
   },
   {
     site: 'Ponte Mahoon',
     location: 'Maehbourg, Mauritius',
     depth: 54,
-    time: 38
+    time: 38,
+    divedOn: new Date("01/18/2009")
   },
   {
     site: 'Molnar Cave',
     location: 'Budapest, Hungary',
     depth: 98,
-    time: 62
+    time: 62,
+    divedOn: new Date("12/7/2008 00:00")
   }];
 }
